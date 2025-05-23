83068bb446500ec617735b5a97b53404aff05e50
Jake Onquit <148852640+sunrayyourdev@users.noreply.github.com>
2025-05-24 03:08:46 +0800

    fix: wrong date locale

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-xl font-semibold mb-4">Day of the Year</h1>
       <span className="text-8xl font-bold">{dayOfYear}</span>
-      <span className="text-lg mt-2 text-gray-500">{today.toLocaleDateString()}</span>
+      <span className="text-lg mt-2 text-gray-500">{today.toLocaleDateString('en-SG')}</span>
     </main>
   );
 }
