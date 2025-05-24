df8163c9f39bca23072f437f03ca74ab620279b1
Jake Onquit <148852640+sunrayyourdev@users.noreply.github.com>
2025-05-25 01:35:46 +0800

    feature: add total days in year

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-xl font-semibold mb-4">Day of the Year</h1>
-      <span className="text-8xl font-bold">{dayOfYear}</span>
+      <span className="text-8xl font-bold">{dayOfYear}/365</span>
       <span className="text-lg mt-2 text-gray-500">
         {today.toLocaleDateString(navigator.language)}{" "}
         {today.toLocaleTimeString(navigator.language, { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' })}
